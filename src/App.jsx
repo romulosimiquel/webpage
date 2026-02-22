4673a526375b919dd7635ca2bdc1a09eba55abbe
romulo <romulo.simiquel@rarolabs.com.br>
2026-02-22 00:27:20 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        Hello sou o Rômulo
+        
+        <div className='Links'>
+          <a href="https://www.linkedin.com/in/romulo-simiquel-lobato/" target="_blank" rel="noopener noreferrer">Linkedin</a>        
+        
+          <a href="https://github.com/romulosimiquel" target="_blank" rel="noopener noreferrer">Github</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
